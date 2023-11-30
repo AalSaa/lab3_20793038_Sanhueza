cc63b892b8100231e1c0821690b376894aaab447
AalSaa <alonso.sanhueza32@gmail.com>
2023-11-29 22:13:51 -0300

    Se ha creado option.

---
+++ src/main/java/Option/Option.java
@@ -0,0 +1,38 @@
+package Option;
+
+import java.util.ArrayList;
+public class Option {
+    private int id;
+    private String message;
+    private int cbCodelink;
+    private int fCodelink;
+    private ArrayList<String> keyword;
+
+    public Option(int id, String message, int cbCodelink, int fCodelink, ArrayList<String> keyword) {
+        this.id = id;
+        this.message = message;
+        this.cbCodelink = cbCodelink;
+        this.fCodelink = fCodelink;
+        this.keyword = keyword;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getCbCodelink() {
+        return cbCodelink;
+    }
+
+    public int getfCodelink() {
+        return fCodelink;
+    }
+
+    public ArrayList<String> getKeyword() {
+        return keyword;
+    }
+}
